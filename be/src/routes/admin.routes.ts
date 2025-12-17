4204efcff735c3c61d2b4f9000bfd08f102637a6
Nguyen Hoang Duy Linh <yudlinh0810@gmail.com>
2025-12-17 14:12:33 +0700

    update(admin): add super_admin for authorizeRoles in route

--- be/src/routes/admin.routes.ts
+++ be/src/routes/admin.routes.ts
@@ -10,38 +10,48 @@ const userController = new UserController();
 const authService = new AuthService();
 
 // create admin
-route.post("/", authService.verifyAccessToken, authorizeRoles("admin"), adminController.create);
+route.post(
+  "/",
+  authService.verifyAccessToken,
+  authorizeRoles("admin", "super_admin"),
+  adminController.create
+);
 // get all admins with pagination
-route.get("/", authService.verifyAccessToken, authorizeRoles("admin"), adminController.getAll);
+route.get(
+  "/",
+  authService.verifyAccessToken,
+  authorizeRoles("admin", "super_admin"),
+  adminController.getAll
+);
 // get admin by id
-route.get("/:id", authService.verifyAccessToken, authorizeRoles("admin"), userController.fetchUser);
+route.get(
+  "/:id",
+  authService.verifyAccessToken,
+  authorizeRoles("admin", "super_admin"),
+  userController.fetchUser
+);
 // update admin details
 route.put(
   "/profile",
   authService.verifyAccessToken,
-  authorizeRoles("admin"),
+  authorizeRoles("admin", "super_admin"),
   adminController.updateAdminDetails
 );
 // update admin password
 route.put(
   "/password",
   authService.verifyAccessToken,
-  authorizeRoles("admin"),
+  authorizeRoles("admin", "super_admin"),
   adminController.updateAdminPassword
 );
 // reset admin password
 route.post(
   "/reset-password",
   authService.verifyAccessToken,
-  authorizeRoles("admin"),
+  authorizeRoles("admin", "super_admin"),
   adminController.resetAdminPassword
 );
 // confirm reset admin password
-route.post(
-  "/confirm-reset-password",
-  authService.verifyAccessToken,
-  authorizeRoles("admin"),
-  adminController.confirmResetAdminPassword
-);
+route.post("/confirm-reset-password", adminController.confirmResetAdminPassword);
 
 export default route;
